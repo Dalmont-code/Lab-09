5bc0dd0cf533bb8eb9837ca28f74884bd4abb045
Lorenzo Dalmonte <lorenzo.dalmonte4@studio.unibo.it>
2022-11-21 12:48:30 +0100

    Esercizio 91, rimossi import inutilizzati

---
+++ 92-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
@@ -0,0 +1,16 @@
+package it.unibo.oop.workers02;
+
+public class MultiThreadedSumMatrix implements SumMatrix {
+
+    private final int nthreads;
+
+    public MultiThreadedSumMatrix(final int nthreads) {
+        this.nthreads = nthreads;
+    }
+
+    @Override
+    public double sum(double[][] matrix) {
+
+        return 0;
+    }
+}
